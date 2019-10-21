fd764373916ef9dcdd10bd0155a840d21fd69e27
ckli76 <ckli@hawaii.edu>
2019-10-20 23:57:46 -1000

    Lab11

---
+++ Lab11/Ex4c.js
@@ -0,0 +1,18 @@
+function isNonNegInt(q)
+{
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+
+    return(errors.length == 0);
+}
+
+attributes  =  "Clement;20;20.5;-19.5" ;
+separator = ";";
+pieces = attributes.split(separator);
+
+for (i=0; i<pieces.length; i++)
+{
+    console.log(`${typeof(pieces[i])} ${pieces[i]} ${isNonNegInt(pieces[i])}`);
+}
